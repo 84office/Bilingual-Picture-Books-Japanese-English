5bde744e26568754b5f6784a7e33d6760a20206b
84office <hiromichi.hashizume@84office.jp>
2025-08-22 00:26:04 +0700

    Fix relative import paths by removing /app/ from paths

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 @@ .. @@
-import type { Book, CreationParams, View } from "./lib/types";
-// ★ サーバーアクションは使わないので削除
-// import { generateStoryAndImages } from './actions';
 -import CreationForm from "./components/CreationForm";
+import LoadingScreen from "@/app/components/LoadingScreen";
+import BookViewer from "@/app/components/BookViewer";
+import Bookshelf from "@/app/components/Bookshelf";
+import CreationForm from "@/app/components/CreationForm";
+import { Header } from "@/app/components/Header";
 import LoadingScreen from "./components/LoadingScreen";
 import BookViewer from "./components/BookViewer";
 import Bookshelf from "./components/Bookshelf";
